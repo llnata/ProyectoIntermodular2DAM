9b10ece30118e4280b8fc020c418b45855fcc004
Lluis Navalon Talens <llnata@xuqueralumnat.es>
2026-03-06 09:38:36 +0100

    Video de la aplicacion subido

--- recordis/data/api.js
+++ recordis/data/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.133.2.60:8080',
+  baseURL: 'http://10.245.164.198:8080',
   timeout: 5000,
   headers: { 'Content-Type': 'application/json' },
 });
